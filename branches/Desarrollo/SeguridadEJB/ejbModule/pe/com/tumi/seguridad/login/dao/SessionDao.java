5a29590002930cf4f043c99c03ceac00f73aac43
cdelosrios@bizarq.com <cdelosrios@bizarq.com@b3a863b4-f5f8-5366-a268-df30542ed8c1>
2014-07-25 02:14:08 +0000

    REQ14-002 - metodo para obtener la ultima sesion activa de un usuario determinado

--- branches/Desarrollo/SeguridadEJB/ejbModule/pe/com/tumi/seguridad/login/dao/SessionDao.java
+++ branches/Desarrollo/SeguridadEJB/ejbModule/pe/com/tumi/seguridad/login/dao/SessionDao.java
@@ -1,5 +1,7 @@
 package pe.com.tumi.seguridad.login.dao;
-
+//Inicio: REQ14-002 - bizarq - 22/07/2014
+import java.util.List;
+//Fin: REQ14-002 - bizarq - 22/07/2014
 import pe.com.tumi.framework.negocio.exception.DAOException;
 import pe.com.tumi.framework.negocio.persistencia.dao.TumiDao;
 import pe.com.tumi.seguridad.login.domain.Session;
@@ -8,4 +10,7 @@ public interface SessionDao extends TumiDao {
 	public Session grabar(Session o) throws DAOException;
 	public Session modificar(Session o) throws DAOException;
 	public Integer getCntActiveSessionsByUser(Object o) throws DAOException;
+	//Inicio: REQ14-002 - bizarq - 22/07/2014
+	public List<Session> getListSesionByUser (Object o) throws DAOException;
+	//Fin: REQ14-002 - bizarq - 22/07/2014
 }
