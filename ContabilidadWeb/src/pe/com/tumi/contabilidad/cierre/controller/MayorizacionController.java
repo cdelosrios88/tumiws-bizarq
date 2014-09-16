24ef97da14209b4a6bb56180f7b7cbb5d1769c70
cdelosrios@bizarq.com <cdelosrios@bizarq.com@b3a863b4-f5f8-5366-a268-df30542ed8c1>
2014-09-16 05:09:45 +0000

    Modified by cdelosrios on 16/09/2014 0009

--- ContabilidadWeb/src/pe/com/tumi/contabilidad/cierre/controller/MayorizacionController.java
+++ ContabilidadWeb/src/pe/com/tumi/contabilidad/cierre/controller/MayorizacionController.java
@@ -1,5 +1,7 @@
 package pe.com.tumi.contabilidad.cierre.controller;
 
+import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 
 import javax.faces.context.FacesContext;
@@ -7,130 +9,24 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.apache.log4j.Logger;
 
-import pe.com.tumi.contabilidad.cierre.domain.LibroMayor;
+import pe.com.tumi.common.util.Constante;
+import pe.com.tumi.framework.negocio.ejb.factory.EJBFactory;
+import pe.com.tumi.parametro.tabla.domain.Tabla;
 import pe.com.tumi.seguridad.login.domain.Usuario;
+import pe.com.tumi.seguridad.permiso.domain.Password;
+import pe.com.tumi.seguridad.permiso.domain.PasswordId;
+import pe.com.tumi.seguridad.permiso.facade.PermisoFacadeRemote;
 
 public class MayorizacionController {
 
 	protected static Logger log;
 	private Usuario usuario;
-	private List listaLibroMayor;
-	private LibroMayor libroMayorFiltro;
-	private LibroMayor libroMayorNuevo;
-	private List listaAnios;
-	private boolean mostrarBtnEliminar;
-	private boolean mostrarMensajeExito;
-	private boolean mostrarMensajeError;
-	private boolean deshabilitarNuevo;
-	private boolean mostrarPanelInferior;
-	private boolean registrarNuevo;
-	private boolean habilitarGrabar;
-	private boolean seleccionaRegistro;
+	private String strPassword;
+	private String strMsgError;
+	private List<Tabla> listaAnios;
 	
-	public boolean isMostrarBtnEliminar() {
-		return mostrarBtnEliminar;
-	}
-
-	public void setMostrarBtnEliminar(boolean mostrarBtnEliminar) {
-		this.mostrarBtnEliminar = mostrarBtnEliminar;
-	}
-
-	public boolean isMostrarMensajeExito() {
-		return mostrarMensajeExito;
-	}
-
-	public void setMostrarMensajeExito(boolean mostrarMensajeExito) {
-		this.mostrarMensajeExito = mostrarMensajeExito;
-	}
-
-	public boolean isMostrarMensajeError() {
-		return mostrarMensajeError;
-	}
-
-	public void setMostrarMensajeError(boolean mostrarMensajeError) {
-		this.mostrarMensajeError = mostrarMensajeError;
-	}
-
-	public boolean isDeshabilitarNuevo() {
-		return deshabilitarNuevo;
-	}
-
-	public void setDeshabilitarNuevo(boolean deshabilitarNuevo) {
-		this.deshabilitarNuevo = deshabilitarNuevo;
-	}
-
-	public boolean isMostrarPanelInferior() {
-		return mostrarPanelInferior;
-	}
-
-	public void setMostrarPanelInferior(boolean mostrarPanelInferior) {
-		this.mostrarPanelInferior = mostrarPanelInferior;
-	}
-
-	public boolean isRegistrarNuevo() {
-		return registrarNuevo;
-	}
-
-	public void setRegistrarNuevo(boolean registrarNuevo) {
-		this.registrarNuevo = registrarNuevo;
-	}
-
-	public boolean isHabilitarGrabar() {
-		return habilitarGrabar;
-	}
-
-	public void setHabilitarGrabar(boolean habilitarGrabar) {
-		this.habilitarGrabar = habilitarGrabar;
-	}
-
-	public boolean isSeleccionaRegistro() {
-		return seleccionaRegistro;
-	}
-
-	public void setSeleccionaRegistro(boolean seleccionaRegistro) {
-		this.seleccionaRegistro = seleccionaRegistro;
-	}
-
-	public List getListaAnios() {
-		return listaAnios;
-	}
-
-	public void setListaAnios(List listaAnios) {
-		this.listaAnios = listaAnios;
-	}
-
-	public LibroMayor getLibroMayorFiltro() {
-		return libroMayorFiltro;
-	}
-
-	public void setLibroMayorFiltro(LibroMayor libroMayorFiltro) {
-		this.libroMayorFiltro = libroMayorFiltro;
-	}
-
-	public LibroMayor getLibroMayorNuevo() {
-		return libroMayorNuevo;
-	}
-
-	public void setLibroMayorNuevo(LibroMayor libroMayorNuevo) {
-		this.libroMayorNuevo = libroMayorNuevo;
-	}
-
-	public Usuario getUsuario() {
-		return usuario;
-	}
-
-	public void setUsuario(Usuario usuario) {
-		this.usuario = usuario;
-	}
-
-	public List getListaLibroMayor() {
-		return listaLibroMayor;
-	}
-
-	public void setListaLibroMayor(List listaLibroMayor) {
-		this.listaLibroMayor = listaLibroMayor;
-	}
-
+	private PermisoFacadeRemote permisoFacade;
+	
 	public MayorizacionController(){
 		log = Logger.getLogger(this.getClass());
 		cargarValoresIniciales();
@@ -140,7 +36,9 @@ public class MayorizacionController {
 		try{
 			usuario = (Usuario)getRequest().getSession().getAttribute("usuario");
 			if(usuario!=null){
-				
+				permisoFacade = (PermisoFacadeRemote) EJBFactory.getRemote(PermisoFacadeRemote.class);
+				strMsgError = null;
+				cargarListaAnios();
 			}else{
 				log.error("--Usuario obtenido es NULL.");
 			}
@@ -150,15 +48,60 @@ public class MayorizacionController {
 		}
 	}
 	
+	private void cargarListaAnios(){
+		listaAnios = new ArrayList<Tabla>();
+		Calendar cal=Calendar.getInstance();
+		Tabla tabla = null;
+		for(int i=0;i<4;i++){
+			tabla = new Tabla();
+			int year = cal.get(Calendar.YEAR);
+			cal.add(Calendar.YEAR, 1);
+			tabla.setIntIdDetalle(year);
+			tabla.setStrDescripcion(""+year);
+			listaAnios.add(tabla);
+		}		
+	}
+	
 	protected HttpServletRequest getRequest() {
 		return (HttpServletRequest) FacesContext.getCurrentInstance().getExternalContext().getRequest();
 	}
 	
 	public String autenticar(){
 		String outcome = null;
-		libroMayorFiltro = new LibroMayor();
-		libroMayorNuevo = new LibroMayor();
-		outcome = "mayorizacion.formulario";
+		Password password = null;
+		strMsgError = null;
+		try {
+			password = new Password();
+			password.setId(new PasswordId());
+			password.getId().setIntEmpresaPk(usuario.getEmpresa().getIntIdEmpresa());
+			password.getId().setIntIdTransaccion(Constante.INT_IDTRANSACCION_MAYORIZACION);
+			password.setStrContrasena(strPassword);
+			password = permisoFacade.getPasswordPorPkYPass(password);
+			log.info("password: " + password);
+			if(password!=null){
+				outcome = "mayorizacion.formulario";
+			}else {
+				strMsgError = "Clave incorrecta. Favor verificar";
+			}
+		} catch (Exception e) {
+			log.error(e.getMessage(), e);
+		}
 		return outcome;
+	}
+
+	public String getStrPassword() {
+		return strPassword;
+	}
+
+	public void setStrPassword(String strPassword) {
+		this.strPassword = strPassword;
+	}
+
+	public String getStrMsgError() {
+		return strMsgError;
+	}
+
+	public void setStrMsgError(String strMsgError) {
+		this.strMsgError = strMsgError;
 	}
 }
