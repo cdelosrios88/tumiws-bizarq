dca46b74d61da5dee97f2edc0da56c3a34abede8
cdelosrios@bizarq.com <cdelosrios@bizarq.com@b3a863b4-f5f8-5366-a268-df30542ed8c1>
2014-09-16 05:18:58 +0000

    Modified by cdelosrios on 16/09/2014 0019

---
+++ ContabilidadEJB/ejbModule/pe/com/tumi/common/util/CommonUtils.java
@@ -0,0 +1,89 @@
+package pe.com.tumi.common.util;
+
+/**
+ * @author Bizarq Technologies
+ * Cod Req: REQ14-004
+ * Descripción: Clase utilitaria que albergará los métodos más comunes usados en la implementación
+ * 				de métodos tales como fechas, periodos, sucursal, sub sucursal, etc.
+ * Fecha: 24/05/2014
+ * */
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+
+import javax.faces.model.SelectItem;
+
+import org.apache.log4j.Logger;
+
+import pe.com.tumi.empresa.domain.Sucursal;
+import pe.com.tumi.framework.negocio.ejb.factory.EJBFactory;
+import pe.com.tumi.seguridad.empresa.facade.EmpresaFacadeRemote;
+
+public class CommonUtils {
+	
+	protected static Logger log;
+	
+	public CommonUtils(){
+		log = Logger.getLogger(this.getClass());
+		
+	}
+	
+	/**
+	 * @author Christian De los Ríos
+	 * @param  Ninguno
+	 * @return List<SelectItem> : Retorna una lista con un año menos al actual y 
+	 * 								5 posteriores desde el presente 
+	 * */
+	public static final List<SelectItem> getListAnios() {
+		List<SelectItem> listYears = new ArrayList<SelectItem>(); 
+		try {
+			int year=Calendar.getInstance().get(Calendar.YEAR)+5;
+			int cont=0;
+
+			for(int j=year; j>=year-6; j--){
+				cont++;
+			}			
+			for(int i=0; i<cont; i++){
+				listYears.add(i, new SelectItem(year));
+				year--;
+			}	
+		} catch (Exception e) {
+			log.error("Error en getListYears ---> "+e);
+		}
+		return listYears;
+	}
+	
+	public static final Integer concatPeriodo(Integer anio, Integer mes){
+		Integer intPeriodo = 0;
+		String strMes="";
+		if (mes.compareTo(10)<0) {
+			strMes="0"+mes;
+		}else{
+			strMes=mes.toString();
+		}
+		intPeriodo = Integer.valueOf(anio+strMes);
+		return intPeriodo;
+	}
+	
+	/**
+	 * @author Christian De los Ríos - Bizarq
+	 * Descripción:
+	 * Método que retorna el ID correcto de una determinada sucursal de acuerdo al ID Persona
+	 * @param 
+	 * 		<Integer>intIdPersona<Integer>
+	 * @return retorna el Id Sucursal de acuerdo a su PK Jurìdica.
+	 * */
+	public static final Integer getSucursalIdByPkPersona(Integer intIdPersona){
+		Integer intIdSucursal = null;
+		Sucursal sucursal = null;
+		try {
+			EmpresaFacadeRemote localEmpresa = (EmpresaFacadeRemote)EJBFactory.getRemote(EmpresaFacadeRemote.class);
+			sucursal = localEmpresa.getSucursalPorIdPersona(intIdPersona);
+			intIdSucursal = sucursal.getId().getIntIdSucursal();
+		} catch (Exception e) {
+			log.error(e.getMessage(), e);
+		}
+		return intIdSucursal;
+	}
+}
