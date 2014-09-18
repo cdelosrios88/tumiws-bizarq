68c7919989c44d2e08b2882b5c53de2702da1ec9
cdelosrios@bizarq.com <cdelosrios@bizarq.com@b3a863b4-f5f8-5366-a268-df30542ed8c1>
2014-09-18 01:15:27 +0000

    Modified by cdelosrios on 17/09/2014 for REQ14-004

--- branches/Desarrollo/ContabilidadEJB/ejbModule/pe/com/tumi/contabilidad/core/facade/MayorizacionFacadeLocal.java
+++ branches/Desarrollo/ContabilidadEJB/ejbModule/pe/com/tumi/contabilidad/core/facade/MayorizacionFacadeLocal.java
@@ -1,9 +1,11 @@
 /**
- * @author Bizarq
- * Cod. Req: REQ14-004: Proceso Mayorización
- * Created: 16/09/2014
- * */
-
+* Resumen.
+* Objeto: MayorizacionFacade
+* Descripción:  Facade principal del proceso de mayorización.
+* Fecha de Creación: 17/09/2014.
+* Requerimiento de Creación: REQ14-004
+* Autor: Bizarq
+*/
 package pe.com.tumi.contabilidad.core.facade;
 
 import pe.com.tumi.framework.negocio.exception.BusinessException;
