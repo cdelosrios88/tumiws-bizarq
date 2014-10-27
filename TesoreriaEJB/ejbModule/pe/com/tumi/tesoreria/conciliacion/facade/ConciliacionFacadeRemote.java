c3517d6ab55532f363ce46af6fcc776b9890db0d
cdelosrios@bizarq.com <cdelosrios@bizarq.com@b3a863b4-f5f8-5366-a268-df30542ed8c1>
2014-10-27 05:38:00 +0000

    RSIS14-006 - CGD

---
+++ TesoreriaEJB/ejbModule/pe/com/tumi/tesoreria/conciliacion/facade/ConciliacionFacadeRemote.java
@@ -0,0 +1,13 @@
+/**
+* Resumen.
+* Objeto: ConciliacionFacade
+* Descripción:  Facade principal del proceso de conciliacion bancaria.
+* Fecha de Creación: 18/10/2014.
+* Requerimiento de Creación: REQ14-006
+* Autor: Bizarq
+*/
+package pe.com.tumi.tesoreria.conciliacion.facade;
+
+public interface ConciliacionFacadeRemote {
+
+}
