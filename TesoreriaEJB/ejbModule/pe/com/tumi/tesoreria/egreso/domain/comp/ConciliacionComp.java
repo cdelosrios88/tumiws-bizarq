aef52cb749575d45d6976208ac1a761476d6d9e3
cdelosrios@bizarq.com <cdelosrios@bizarq.com@b3a863b4-f5f8-5366-a268-df30542ed8c1>
2014-10-27 06:40:53 +0000

    RSIS14-006 - CGD

---
+++ TesoreriaEJB/ejbModule/pe/com/tumi/tesoreria/egreso/domain/comp/ConciliacionComp.java
@@ -0,0 +1,216 @@
+/************************************************************************
+/* Nombre de componente: ConciliacionComp 
+ * Descripción: Clase Compuesta de Conciliacion
+ * del aplicativo - Modulo Tesoreria
+ * Cod. Req.: REQ14-006  
+ * Autor : Bizarq Technologies 
+ * Versión : v1.0 - Creacion de componente 
+ * Fecha creación : 18/10/2014 
+/* ********************************************************************* */
+package pe.com.tumi.tesoreria.egreso.domain.comp;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+import pe.com.tumi.framework.negocio.domain.TumiDomain;
+
+public class ConciliacionComp extends TumiDomain {
+	
+
+	//Filtro de la grilla de busqueda de concniliacion.
+	private Date dtBusqFechaDesde;
+	
+	private Date dtBusqFechaHasta;
+	
+	private Integer intBusqPersEmpresa;
+
+	private Integer intBusqItemBancoFondo;
+	
+	private Integer intBusqItemBancoCuenta;
+	
+	
+	
+	// Campos de Resultado de busqueda
+	
+	private Integer intEmpresaConciliacion;
+	
+	private Integer intItemConciliacion;	
+	
+	private String strFechaConciliacion;
+	
+	private String strBanco;
+	
+	private String strTipoCuenta;
+	
+	private String strMoneda;
+	
+	private String strNumeroCuenta;
+	
+	private BigDecimal bdSaldoAnterior;
+	
+	private BigDecimal bdDebe;
+	
+	private BigDecimal bdHaber;
+	
+	private BigDecimal bdSaldoCaja;
+	
+	private BigDecimal bdSaldoConciliacion;
+	
+	private Integer intNroMovimientos;
+	
+	private BigDecimal bdPorConciliar;
+
+	public Date getDtBusqFechaDesde() {
+		return dtBusqFechaDesde;
+	}
+
+	public void setDtBusqFechaDesde(Date dtBusqFechaDesde) {
+		this.dtBusqFechaDesde = dtBusqFechaDesde;
+	}
+
+	public Date getDtBusqFechaHasta() {
+		return dtBusqFechaHasta;
+	}
+
+	public void setDtBusqFechaHasta(Date dtBusqFechaHasta) {
+		this.dtBusqFechaHasta = dtBusqFechaHasta;
+	}
+
+	public Integer getIntBusqPersEmpresa() {
+		return intBusqPersEmpresa;
+	}
+
+	public void setIntBusqPersEmpresa(Integer intBusqPersEmpresa) {
+		this.intBusqPersEmpresa = intBusqPersEmpresa;
+	}
+
+	public Integer getIntBusqItemBancoFondo() {
+		return intBusqItemBancoFondo;
+	}
+
+	public void setIntBusqItemBancoFondo(Integer intBusqItemBancoFondo) {
+		this.intBusqItemBancoFondo = intBusqItemBancoFondo;
+	}
+
+	public Integer getIntBusqItemBancoCuenta() {
+		return intBusqItemBancoCuenta;
+	}
+
+	public void setIntBusqItemBancoCuenta(Integer intBusqItemBancoCuenta) {
+		this.intBusqItemBancoCuenta = intBusqItemBancoCuenta;
+	}
+
+	public Integer getIntEmpresaConciliacion() {
+		return intEmpresaConciliacion;
+	}
+
+	public void setIntEmpresaConciliacion(Integer intEmpresaConciliacion) {
+		this.intEmpresaConciliacion = intEmpresaConciliacion;
+	}
+
+	public Integer getIntItemConciliacion() {
+		return intItemConciliacion;
+	}
+
+	public void setIntItemConciliacion(Integer intItemConciliacion) {
+		this.intItemConciliacion = intItemConciliacion;
+	}
+
+	public String getStrFechaConciliacion() {
+		return strFechaConciliacion;
+	}
+
+	public void setStrFechaConciliacion(String strFechaConciliacion) {
+		this.strFechaConciliacion = strFechaConciliacion;
+	}
+
+	public String getStrBanco() {
+		return strBanco;
+	}
+
+	public void setStrBanco(String strBanco) {
+		this.strBanco = strBanco;
+	}
+
+	public String getStrTipoCuenta() {
+		return strTipoCuenta;
+	}
+
+	public void setStrTipoCuenta(String strTipoCuenta) {
+		this.strTipoCuenta = strTipoCuenta;
+	}
+
+	public String getStrMoneda() {
+		return strMoneda;
+	}
+
+	public void setStrMoneda(String strMoneda) {
+		this.strMoneda = strMoneda;
+	}
+
+	public String getStrNumeroCuenta() {
+		return strNumeroCuenta;
+	}
+
+	public void setStrNumeroCuenta(String strNumeroCuenta) {
+		this.strNumeroCuenta = strNumeroCuenta;
+	}
+
+	public BigDecimal getBdSaldoAnterior() {
+		return bdSaldoAnterior;
+	}
+
+	public void setBdSaldoAnterior(BigDecimal bdSaldoAnterior) {
+		this.bdSaldoAnterior = bdSaldoAnterior;
+	}
+
+	public BigDecimal getBdDebe() {
+		return bdDebe;
+	}
+
+	public void setBdDebe(BigDecimal bdDebe) {
+		this.bdDebe = bdDebe;
+	}
+
+	public BigDecimal getBdHaber() {
+		return bdHaber;
+	}
+
+	public void setBdHaber(BigDecimal bdHaber) {
+		this.bdHaber = bdHaber;
+	}
+
+	public BigDecimal getBdSaldoCaja() {
+		return bdSaldoCaja;
+	}
+
+	public void setBdSaldoCaja(BigDecimal bdSaldoCaja) {
+		this.bdSaldoCaja = bdSaldoCaja;
+	}
+
+	public BigDecimal getBdSaldoConciliacion() {
+		return bdSaldoConciliacion;
+	}
+
+	public void setBdSaldoConciliacion(BigDecimal bdSaldoConciliacion) {
+		this.bdSaldoConciliacion = bdSaldoConciliacion;
+	}
+
+	public Integer getIntNroMovimientos() {
+		return intNroMovimientos;
+	}
+
+	public void setIntNroMovimientos(Integer intNroMovimientos) {
+		this.intNroMovimientos = intNroMovimientos;
+	}
+
+	public BigDecimal getBdPorConciliar() {
+		return bdPorConciliar;
+	}
+
+	public void setBdPorConciliar(BigDecimal bdPorConciliar) {
+		this.bdPorConciliar = bdPorConciliar;
+	}
+
+
+}
