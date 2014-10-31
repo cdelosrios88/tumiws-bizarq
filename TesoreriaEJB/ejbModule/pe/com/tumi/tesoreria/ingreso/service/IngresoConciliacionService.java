2bdbd6d21dcbd1b7355ff5e4990465ec950c7043
cdelosrios@bizarq.com <cdelosrios@bizarq.com@b3a863b4-f5f8-5366-a268-df30542ed8c1>
2014-10-31 07:44:37 +0000

    RSIS14-006 - CGD

---
+++ TesoreriaEJB/ejbModule/pe/com/tumi/tesoreria/ingreso/service/IngresoConciliacionService.java
@@ -0,0 +1,13 @@
+package pe.com.tumi.tesoreria.ingreso.service;
+
+public class IngresoConciliacionService {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
