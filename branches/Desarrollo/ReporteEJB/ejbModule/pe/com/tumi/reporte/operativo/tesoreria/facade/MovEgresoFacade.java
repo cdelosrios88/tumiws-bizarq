46eb8f0b2dfcf96b921983db8dab00409a797fcf
cdelosrios@bizarq.com <cdelosrios@bizarq.com@b3a863b4-f5f8-5366-a268-df30542ed8c1>
2014-12-18 07:12:43 +0000

    Modified by cdelosrios on 1712/2014 for REQ14-010

--- branches/Desarrollo/ReporteEJB/ejbModule/pe/com/tumi/reporte/operativo/tesoreria/facade/MovEgresoFacade.java
+++ branches/Desarrollo/ReporteEJB/ejbModule/pe/com/tumi/reporte/operativo/tesoreria/facade/MovEgresoFacade.java
@@ -1,32 +1,23 @@
 package pe.com.tumi.reporte.operativo.tesoreria.facade;
-/* -----------------------------------------------------------------------------------------------------------
-* Modificaciones
-* Motivo                      Fecha            Nombre                      Descripción
-* -----------------------------------------------------------------------------------------------------------
-* REQ14-009       			15/12/2014     Christian De los Ríos        Creaciòn de componente         
-*/
 
 import java.util.List;
 
 import javax.ejb.Local;
 import javax.ejb.Remote;
 import javax.ejb.Stateless;
-import javax.faces.model.SelectItem;
 
 import org.apache.log4j.Logger;
 
 import pe.com.tumi.framework.negocio.exception.BusinessException;
-import pe.com.tumi.framework.negocio.exception.DAOException;
 import pe.com.tumi.framework.negocio.facade.TumiFacade;
 import pe.com.tumi.framework.negocio.factory.TumiFactory;
 import pe.com.tumi.reporte.operativo.tesoreria.bo.MovEgresoBO;
 import pe.com.tumi.reporte.operativo.tesoreria.domain.EgresoFondoFijo;
 import pe.com.tumi.reporte.operativo.tesoreria.domain.MovEgreso;
+import pe.com.tumi.reporte.operativo.tesoreria.domain.MovEgresoDetalle;
 
 /**
- * Clase de acceso Facade con implementacion Local y Remote
- * 
- * @author Bizarq
+ * Session Bean implementation class IngresoCajaFacade
  */
 @Stateless
 @Local(MovEgresoFacadeLocal.class)
@@ -42,17 +33,6 @@ public class MovEgresoFacade extends TumiFacade implements MovEgresoFacadeRemote
         super();
     }
     
-    /**
-	 * Metodo que recupera la lista de Egresos realizados a caja
-	 * segun los filtros ingresados.
-	 * 
-	 * @param intSucursal, Identificador de la sucursal.
-	 * @param intAnio, Anio de consulta.
-	 * @param intTipoFondoFijo, Indicador de tipo de fondo fijo.
-	 * @return Lista de entidades del tipo EgresosCaja.
-	 * 
-	 * @throws BusinessException
-	 */
     public List<MovEgreso> getListFondoFijo(int intSucursal,int intAnio, int intTipoFondoFijo) throws BusinessException{
 		List<MovEgreso> lista = null;
 		try {
@@ -65,7 +45,6 @@ public class MovEgresoFacade extends TumiFacade implements MovEgresoFacadeRemote
 		return lista;
 	}
     
-
     public MovEgreso getListEgresoById(MovEgreso o) throws BusinessException{
 		MovEgreso domain = null;
 		try {
@@ -78,15 +57,18 @@ public class MovEgresoFacade extends TumiFacade implements MovEgresoFacadeRemote
 		return domain;
 	}
     
-    /**
-	 * Metodo que recupera la lista de Egresos de monto fijo realizados a caja
-	 * segun los filtros ingresados.
-	 * 
-	 * @param objMovEgreso, Entidad de tipo Moviemiento Egreso.
-	 * @return Lista de entidades del tipo EgresoFondoFijo.
-	 * 
-	 * @throws BusinessException
-	 */
+    public List<MovEgresoDetalle> getListEgresoDetalleById(MovEgreso o) throws BusinessException{
+		List<MovEgresoDetalle> lista = null;
+		try {
+			lista = boMovEgreso.getListEgresoDetalleById(o);
+		} catch(BusinessException e){
+			throw e;
+		}catch(Exception e){
+			throw new BusinessException(e);
+		}
+		return lista;
+	}
+    
     public List<EgresoFondoFijo> getEgresos (MovEgreso objMovEgreso) throws BusinessException {
     	List<EgresoFondoFijo> lista  = null;
     	try {
