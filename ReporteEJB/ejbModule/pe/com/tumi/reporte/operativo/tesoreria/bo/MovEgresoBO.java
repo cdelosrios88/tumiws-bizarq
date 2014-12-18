6a96231ef3c24d3ac548730c63d6df016d7e7337
cdelosrios@bizarq.com <cdelosrios@bizarq.com@b3a863b4-f5f8-5366-a268-df30542ed8c1>
2014-12-18 06:38:11 +0000

    Modified by cdelosrios on 1712/2014 for REQ14-010

--- ReporteEJB/ejbModule/pe/com/tumi/reporte/operativo/tesoreria/bo/MovEgresoBO.java
+++ ReporteEJB/ejbModule/pe/com/tumi/reporte/operativo/tesoreria/bo/MovEgresoBO.java
@@ -3,7 +3,6 @@ package pe.com.tumi.reporte.operativo.tesoreria.bo;
 import java.util.HashMap;
 import java.util.List;
 
-
 import org.apache.log4j.Logger;
 
 import pe.com.tumi.framework.negocio.exception.BusinessException;
@@ -32,6 +31,33 @@ public class MovEgresoBO {
 			throw new BusinessException(e);
 		}
 		return lista;
+	}
+	
+	public MovEgreso getListEgresoById(MovEgreso o) throws BusinessException{
+		MovEgreso domain = null;
+		List<MovEgreso> lista = null;
+		try{
+			HashMap<String,Object> mapa = new HashMap<String,Object>();
+			mapa.put("intPersEmpresaEgreso", o.getIntEmpresaEgreso());
+			mapa.put("intItemEgresoGeneral", o.getIntItemEgresoGeneral());
+			lista = dao.getListEgresoById(mapa);
+			if(lista!=null){
+				if(lista.size()==1){
+				   domain = lista.get(0);
+				}else if(lista.size()==0){
+				   domain = null;
+				}else{
+				   throw new BusinessException("Obtención de mas de un registro coincidente");
+				}
+			}
+		}catch(DAOException e){
+			throw new BusinessException(e);
+		}catch(BusinessException e){
+			throw e;
+		}catch(Exception e) {
+			throw new BusinessException(e);
+		}
+		return domain;
 	}
 	public List<EgresoFondoFijo> getEgresos (MovEgreso objMovEgreso) throws BusinessException {
 		List<EgresoFondoFijo> lista  = null;
