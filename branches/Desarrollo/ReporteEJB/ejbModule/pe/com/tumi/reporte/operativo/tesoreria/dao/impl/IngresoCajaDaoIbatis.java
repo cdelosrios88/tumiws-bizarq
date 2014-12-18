edc760362e80d74c876626b9aeb290680cbe6589
cdelosrios@bizarq.com <cdelosrios@bizarq.com@b3a863b4-f5f8-5366-a268-df30542ed8c1>
2014-12-18 00:59:42 +0000

    Modified by cdelosrios on 30/11/2014 for REQ14-009

--- branches/Desarrollo/ReporteEJB/ejbModule/pe/com/tumi/reporte/operativo/tesoreria/dao/impl/IngresoCajaDaoIbatis.java
+++ branches/Desarrollo/ReporteEJB/ejbModule/pe/com/tumi/reporte/operativo/tesoreria/dao/impl/IngresoCajaDaoIbatis.java
@@ -4,11 +4,12 @@ import java.util.List;
 
 import pe.com.tumi.framework.negocio.exception.DAOException;
 import pe.com.tumi.framework.negocio.persistencia.dao.impl.TumiDaoIbatis;
-import pe.com.tumi.reporte.operativo.credito.asociativo.domain.Asociativo;
+import pe.com.tumi.reporte.operativo.tesoreria.dao.IngresoCajaDao;
+import pe.com.tumi.reporte.operativo.tesoreria.domain.IngresoCaja;
 
-public class IngresoCajaDaoIbatis extends TumiDaoIbatis {
-	public List<Asociativo> getListaIngresosByTipoIngreso(Object o) throws DAOException {
-		List<Asociativo> lista = null;
+public class IngresoCajaDaoIbatis extends TumiDaoIbatis implements IngresoCajaDao{
+	public List<IngresoCaja> getListaIngresosByTipoIngreso(Object o) throws DAOException {
+		List<IngresoCaja> lista = null;
 		try{
 			lista = (List) getSqlMapClientTemplate().queryForList(getNameSpace() + ".getListIngresosByTipo", o);
 		}catch(Exception e) {
