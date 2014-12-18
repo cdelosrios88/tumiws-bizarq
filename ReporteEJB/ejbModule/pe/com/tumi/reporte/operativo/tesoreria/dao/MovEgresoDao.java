9305da7f2af1ef56054634206b5d319e6c1bf160
cdelosrios@bizarq.com <cdelosrios@bizarq.com@b3a863b4-f5f8-5366-a268-df30542ed8c1>
2014-12-18 06:36:55 +0000

    Modified by cdelosrios on 1712/2014 for REQ14-010

--- ReporteEJB/ejbModule/pe/com/tumi/reporte/operativo/tesoreria/dao/MovEgresoDao.java
+++ ReporteEJB/ejbModule/pe/com/tumi/reporte/operativo/tesoreria/dao/MovEgresoDao.java
@@ -6,8 +6,11 @@ import pe.com.tumi.framework.negocio.exception.DAOException;
 import pe.com.tumi.framework.negocio.persistencia.dao.TumiDao;
 import pe.com.tumi.reporte.operativo.tesoreria.domain.EgresoFondoFijo;
 import pe.com.tumi.reporte.operativo.tesoreria.domain.MovEgreso;
+import pe.com.tumi.reporte.operativo.tesoreria.domain.MovEgresoDetalle;
 
 public interface MovEgresoDao extends TumiDao {
 	public List<MovEgreso> getListFondoFijo(Object o) throws DAOException;
+	public List<MovEgreso> getListEgresoById(Object o) throws DAOException;
+	public List<MovEgresoDetalle> getListEgresoDetalleById(Object o) throws DAOException;
 	public List<EgresoFondoFijo> getEgresos (Object objMovEgreso) throws DAOException;
 }
